32112e688f69c0d3c4cc0dadcc8e8405ba16bf04
Todd Farrell <todd@1251.org>
2013-08-04 19:32:28 +1000

    Verify password if hash given as second argument

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "bcrypt.h"
 
 int main(int argc, char *argv[])
@@ -8,16 +9,32 @@ int main(int argc, char *argv[])
     char salt[BCRYPT_HASHSIZE];
     char hash[BCRYPT_HASHSIZE];
 
-    if (argc != 2)
+    if (argc != 2 && argc != 3)
     {
-        fprintf(stderr, "usage: %s <plaintext>\n", argv[0]);
+        fprintf(stderr, "usage: %s <plaintext> [<salt>]\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
-    bcrypt_gensalt(12, salt);
-    bcrypt_hashpw(argv[1], salt, hash);
-
-    printf("%s\n", hash);
+    if (argc == 2) 
+    {
+        bcrypt_gensalt(12, salt);
+        bcrypt_hashpw(argv[1], salt, hash);
+        printf("%s\n", hash);
+    }
+    else 
+    {
+        bcrypt_hashpw(argv[1], argv[2], hash);
+        printf("Expected: %s\n", argv[2]);
+        printf("  Actual: %s\n", hash);
+        if (strcmp(argv[2], hash) == 0) 
+        {
+            printf("The password matches\n");
+        }
+        else 
+        {
+            printf("The password does NOT match\n");
+        }
+    }
     
     return 0;
 }
